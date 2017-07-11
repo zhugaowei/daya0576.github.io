d87812a2a84d9fe20571c36402d149f48ee4bcc7
daya0576 <daya0576@gmail.com>
2017-07-11 13:06:02 +0800

    backup the whole website

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-rvm use 1.9.3
- 
 sudo systemsetup -settimezone Australia/Sydney
 # sudo timedatectl set-timezone Australia/Sydney
 
-rake generate; rake deploy
+rake generate
+rake deploy
 
 # sudo timedatectl set-timezone Asia/Shanghai
 sudo systemsetup -settimezone Asia/Shanghai
